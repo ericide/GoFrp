6ec3b78ba1c2bee47d4bda510a5e6b076879a3e2
Eric <13197616+ericide@users.noreply.github.com>
2022-10-16 02:08:43 +0800

    Add new method

--- v1/constant/constant.go
+++ v1/constant/constant.go
@@ -4,6 +4,7 @@ const (
 	LInkTypeSignal     byte = 211
 	LInkTypeDataTunnel byte = 212
 
+	MethodSignalAuth          byte = 40
 	MethodApplyNewDataChannel byte = 60
 	MethodPing                byte = 90
 	MethodPong                byte = 91
