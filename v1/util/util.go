44b317a38229ce027fe8cc63f671d85f99fb61a5
Eric <13197616+ericide@users.noreply.github.com>
2022-10-16 02:09:18 +0800

    Use new tool method to package message or read package

--- v1/util/util.go
+++ v1/util/util.go
@@ -1,44 +1,104 @@
 package util
 
-import "errors"
+import (
+	"bytes"
+	"errors"
+	"io"
+)
 
-func CreateDataHeader(index int64, length int, method byte) *[]byte {
-	var pre = make([]byte, 8)
+// pack
+// length message
+//        data
+//        header body
+
+func CreateDataPackage(pwd []byte, index int64, method byte, body []byte) *[]byte {
+	var data = make([]byte, 6)
 	//index
-	pre[0] = byte(index & 0xFF)
-	pre[1] = byte((index >> 8) & 0xFF)
-	pre[2] = byte((index >> 16) & 0xFF)
-	pre[3] = byte((index >> 24) & 0xFF)
-	//length
-	pre[4] = byte(length & 0xFF)
-	pre[5] = byte((length >> 8) & 0xFF)
-	pre[6] = method
-
+	data[0] = byte(index & 0xFF)
+	data[1] = byte((index >> 8) & 0xFF)
+	data[2] = byte((index >> 16) & 0xFF)
+	data[3] = byte((index >> 24) & 0xFF)
+	data[4] = method
+	//sum
 	var csum byte = 0
-	for i := 0; i < 7; i++ {
-		csum += pre[i]
+	for i := 0; i < 5; i++ {
+		csum += data[i]
 	}
-	pre[7] = csum
+	data[5] = csum
 
-	return &pre
+	if body != nil {
+		data = append(data, body...)
+	}
+
+	message := AesEncrypt(data, pwd)
+
+	pack := make([]byte, len(message)+2)
+
+	// fmt.Println("Write Package:", len(message))
+
+	//length
+	pack[0] = byte(len(message) & 0xFF)
+	pack[1] = byte((len(message) >> 8) & 0xFF)
+	copy(pack[2:], message)
+
+	return &pack
 
 }
 
-func VerifyDataHeader(fun []byte) (int64, int, byte, error) {
-	var identity int64 = int64(fun[3])*256*256*256 + int64(fun[2])*256*256 + int64(fun[1])*256 + int64(fun[0])
-	var length int = int(fun[5])*256 + int(fun[4])
-	var method byte = fun[6]
-	var sum byte = fun[7]
+func ReadDataPackage(pwd []byte, conn io.ReadCloser) (int64, byte, []byte, error) {
+	fun := []byte{0, 0}
+	_, err := io.ReadAtLeast(conn, fun, len(fun))
+	// fmt.Println(fun)
+	if err != nil {
+		conn.Close()
+		return 0, 0, nil, err
+	}
+	length := int(fun[1])*256 + int(fun[0])
+
+	// fmt.Println("Read Package:", length)
+
+	fun = make([]byte, length)
+	_, err = io.ReadAtLeast(conn, fun, len(fun))
+	if err != nil {
+		conn.Close()
+		return 0, 0, nil, err
+	}
+	// fmt.Println(fun)
+	return ParseDataMessage(pwd, fun)
+}
+
+func ParseDataMessage(pwd []byte, message []byte) (int64, byte, []byte, error) {
+	data, err := AesDecrypt(message, pwd)
+
+	if err != nil {
+		return 0, 0, nil, errors.New("not correct sum")
+	}
+
+	var identity int64 = int64(data[3])*256*256*256 + int64(data[2])*256*256 + int64(data[1])*256 + int64(data[0])
+
+	var method byte = data[4]
+	var sum byte = data[5]
 
 	var csum byte = 0
-	for i := 0; i < 7; i++ {
-		csum += fun[i]
+	for i := 0; i < 5; i++ {
+		csum += data[i]
 	}
 
 	if sum != csum {
-		return 0, 0, 0, errors.New("not correct sum")
+		return 0, 0, nil, errors.New("not correct sum")
 	}
 
-	return identity, length, method, nil
+	return identity, method, data[6:], nil
 
+}
+
+func ParsePassword(p string) []byte {
+	ori := []byte(p)
+	if len(ori) > 32 {
+		return ori[:32]
+	}
+
+	padding := 32 - len(ori)
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ori, padtext...)
 }
