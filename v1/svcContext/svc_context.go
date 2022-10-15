fe85532c1a0cf38a2f2f5a007ca45c3d9ffb06ee
Eric <13197616+ericide@users.noreply.github.com>
2022-10-16 02:06:09 +0800

    Move svc out of server

--- v1/svcContext/svc_context.go
+++ v1/svcContext/svc_context.go
@@ -8,4 +8,8 @@ type SVCContext struct {
 	ApplyNewDataTunChan chan int64
 	TaskMap             sync.Map
 	ServerPort          int
+	ServerHost          string
+	BindPort            int
+	BindHost            string
+	Password            []byte
 }
