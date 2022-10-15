774396a2abdf2e17e391a2ae473a85eca9787cbe
Eric <13197616+ericide@users.noreply.github.com>
2022-10-16 02:10:19 +0800

    Both server and client use context configuration

--- main.go
+++ main.go
@@ -2,30 +2,37 @@ package main
 
 import (
 	"GoFrp/v1/server"
-	"GoFrp/v1/server/svcContext"
 	"GoFrp/v1/slave"
+	"GoFrp/v1/svcContext"
+	"GoFrp/v1/util"
 	"flag"
 	"sync"
 )
 
 var mode = flag.String("m", "server", "run mode")
-var port = flag.Int("p", 10000, "data port")
-var cmdPort = flag.Int("cp", 10001, "cmd port")
-var remoteCmdHost = flag.String("rch", "localhost:10001", "remote cmd host")
-var localHost = flag.String("lh", "192.168.0.65:8001", "remote cmd host")
+var port = flag.Int("p", 10000, "server bind port")
+var serverHost = flag.String("h", "0.0.0.0", "server host")
+var bindHost = flag.String("lh", "localhost", "local host")
+var bindPort = flag.Int("lp", 443, "local bind port")
+var password = flag.String("pwd", "12345678", "password for connect")
 
 func main() {
+
 	flag.Parse()
 
-	if *mode == "server" {
-		svc := &svcContext.SVCContext{
-			ApplyNewDataTunChan: nil,
-			TaskMap:             sync.Map{},
-			ServerPort:          *port,
-		}
+	svc := &svcContext.SVCContext{
+		ApplyNewDataTunChan: nil,
+		TaskMap:             sync.Map{},
+		ServerPort:          *port,
+		ServerHost:          *serverHost,
+		BindHost:            *bindHost,
+		BindPort:            *bindPort,
+		Password:            util.ParsePassword(*password),
+	}
 
-		server.StartServer(svc)
+	if *mode == "server" {
+		server.Start(svc)
 	} else {
-		slave.Listen(*remoteCmdHost, *localHost)
+		slave.Start(svc)
 	}
 }
