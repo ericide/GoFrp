486fd77d7d94a88ce695f34e526b4c5cae495c76
Eric <13197616+ericide@users.noreply.github.com>
2022-10-18 01:17:01 +0800

    Update build script to support more platform

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 mkdir build
 #export GOARCH=arm64
-env GOOS=linux GOARCH=arm64 go build -o build/mainarm main.go
-env GOOS=linux GOARCH=amd64 go build -o build/mainamd main.go
+env GOOS=linux GOARCH=arm64 go build -o build/frp_linux_arm64 main.go
+env GOOS=linux GOARCH=arm go build -o build/frp_linux_armv7 main.go
+env GOOS=linux GOARCH=amd64 go build -o build/frp_linux_amd64 main.go
+env GOOS=darwin GOARCH=arm64 go build -o build/frp_darwin_arm64 main.go
+env GOOS=darwin GOARCH=amd64 go build -o build/frp_darwin_amd64 main.go
+env GOOS=windows GOARCH=arm64 go build -o build/frp_windows_arm64 main.go
+env GOOS=windows GOARCH=amd64 go build -o build/frp_windows_amd64 main.go
